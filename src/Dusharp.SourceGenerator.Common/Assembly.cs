81c072c4f2c76e278fe92622037ebfc0d8541600
Vitali Kolebyanov <kolebynov18@gmail.com>
2024-12-19 18:33:43 +0400

    fix: Rename `Dusharp` project to `Dusharp.Common`

--- src/Dusharp.SourceGenerator.Common/Assembly.cs
+++ src/Dusharp.SourceGenerator.Common/Assembly.cs
@@ -1,4 +1,4 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("Dusharp.SourceGenerator")]
-[assembly: InternalsVisibleTo("Dusharp")]
+[assembly: InternalsVisibleTo("Dusharp.Common")]
