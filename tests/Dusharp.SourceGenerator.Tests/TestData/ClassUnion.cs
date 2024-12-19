a8306b1c331cad25036ffc820d78fcd0544c8e21
Vitali Kolebyanov <kolebynov18@gmail.com>
2024-12-19 16:04:11 +0400

    test: Add tests for union implementation code generation

---
+++ tests/Dusharp.SourceGenerator.Tests/TestData/ClassUnion.cs
@@ -0,0 +1,18 @@
+using Dusharp;
+
+namespace TestUnion;
+
+[Union]
+public partial class ClassUnion<T1, T2, T3>
+	where T1 : unmanaged
+	where T2 : class
+{
+	[UnionCase]
+	public static partial ClassUnion<T1, T2, T3> Case1();
+
+	[UnionCase]
+	public static partial ClassUnion<T1, T2, T3> Case2(int value1, T1 value2, T2 value3, T3 value4);
+
+	[UnionCase]
+	public static partial ClassUnion<T1, T2, T3> Case3(long? value1, T1? value2, T2? value3, T3? value4, T1 value5);
+}
